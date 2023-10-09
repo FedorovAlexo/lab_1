d912cfcbd77fae059273069fe20d0e80f816c62e
FedorovAlexo <fet18121992@gmail.com>
2023-10-09 20:51:42 +0300

    Solution

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
-name = Aleksandr
-age = 30
-city = Kherson
-country = Ukraine
+name = 'Aleksander'
+age = '30'
+city = 'Kherson'
+country = 'Ukraine'
 
-print(f'My name is {Aleksandr}')
-
+print(f'My name is {name}')
+print(f'I am {age} years old')
+print(f'I am from {city}, {country}')
