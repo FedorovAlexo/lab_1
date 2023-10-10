34e37babed0de2fd17f1614ea08cedc821050ad5
AleksadrFedorov <146239726+FedorovAlexo@users.noreply.github.com>
2023-10-10 14:42:02 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-name = "Anastasiia"
-age =
-city =
-country =
+name = "Aleksandr"
+age = "30"
+city = "Kherson"
+country = "Ukraine"
 
 print(f'My name is {name}')
+print(f'I'm 30 years ago')
+print(f'I'm from Kherson, Ukraine')
